ef1b330017c901298d2cc28ff491ff42634cea9a
Franco Pacheco <pachecozux@gmail.com>
2023-11-22 01:19:34 -0300

    new main scene

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform player; // Referencia al transform del jugador
+
+    void FixedUpdate()
+    {
+        if (player != null)
+        {
+            // Obtener la posición actual de la cámara
+            Vector3 posicionCamara = transform.position;
+
+            // Establecer la posición X de la cámara para seguir al jugador
+            posicionCamara.x = player.position.x;
+
+            // Establecer la posición Y de la cámara para seguir al jugador
+            posicionCamara.y = player.position.y;
+
+            // Asignar la nueva posición a la cámara
+            transform.position = posicionCamara;
+        }
+    }
+}
